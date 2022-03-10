81e57ba42ab43dcbccfdacf98986739c7f09ac8e
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-03-11 00:47:31 +0200

    Laborator 1: exercitiul 2: Add solution for exercise 5
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/reprezentare-numere/5-odd_even/odd_even.c
+++ laborator/content/reprezentare-numere/5-odd_even/odd_even.c
@@ -3,21 +3,33 @@
 
 void print_binary(int number, int nr_bits)
 {
-	/* TODO */
-	(void) number;
-	(void) nr_bits;
+	for(int i = nr_bits; i >= 0; i--) {
+		int c = number >> i;
+	if( c & 1)
+		printf("1");
+	else
+		printf("0");
+	}
+	printf("\n");
 }
 
 void check_parity(int *numbers, int n)
 {
-	/* TODO */
-	(void) numbers;
-	(void) n;
+		for(int i = 0; i < n; i++)
+		if((*(numbers + i) ^ 1 )== *(numbers +i) +1)
+			print_binary(*(numbers + i), 8 * sizeof(int));
+		else 
+			printf("0x%x\n", *(numbers + i));
 }
 
 int main()
 {
-	/* TODO: Test functions */
+	int nr, *numbers;
+	scanf("%d", &nr);
+	numbers = malloc(nr * sizeof(int));
+	for(int i = 0; i < nr; i++)
+		scanf("%d", numbers+i);
+	check_parity(numbers, nr);
 
 	return 0;
 }
