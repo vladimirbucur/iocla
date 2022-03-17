f6527f90d4665c9926fcb14c40a7b84220a98bb6
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-03-17 15:45:35 +0200

    I compile the code with the flag -g. After I started executable, I set a breakpoin to main and I noticed how data is represented in the memory.
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex6/ex6.c
+++ laborator/content/operatii-memorie-gdb/ex6/ex6.c
@@ -5,7 +5,6 @@ int main() {
 	int b = -27714;
 	short c = 1475;
 	int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
-
 	unsigned int *int_ptr = (unsigned int *) &v;
 
 	for (int i = 0 ; i < sizeof(v) / sizeof(*int_ptr) ; ++i) {
