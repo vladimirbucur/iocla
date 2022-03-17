9ff684d42da1c229ef9623d852ea896e896aba60
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-03-17 15:29:32 +0200

    Laborator 2 : exercitiu 1: Add solutions for exercise 1
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex1/ex1.c
+++ laborator/content/operatii-memorie-gdb/ex1/ex1.c
@@ -8,7 +8,26 @@
  */
 
 int main() {
+    //PARCURGEREA DIN 1 IN 1
+    // int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+    // unsigned char *char_ptr = (char *)v;
+    // for (int i = 0; i < sizeof(v) / sizeof(unsigned char); i++ ) {
+    //     printf("%p -> 0x%x\n", char_ptr + i, *(char_ptr + i));
+    // }
+
+    //PARCURGEREA DIN 2 IN 2
+    // int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+    // short *short_ptr = (short *)v;
+    // for (int i = 0; i < sizeof(v) / sizeof(short); i++ ) {
+    //     printf("%p -> 0x%x\n", short_ptr + i, *(short_ptr + i));
+    // }
+
+    //PARCURGEREA DIN 4 IN 4
     int v[] = {0xCAFEBABE, 0xDEADBEEF, 0x0B00B135, 0xBAADF00D, 0xDEADC0DE};
+    int *int_ptr = (int *)v;
+    for (int i = 0; i < sizeof(v) / sizeof(int); i++ ) {
+        printf("%p -> 0x%x\n", int_ptr, *(int_ptr + i));
+    }
 
     return 0;
 }
