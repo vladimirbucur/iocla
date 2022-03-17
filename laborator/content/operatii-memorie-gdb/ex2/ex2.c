7636e6e05d18e077c4c5d591f963dd8b4716b6cd
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-03-17 15:34:00 +0200

    Laborator 1 : exerciriu 2: Add solutions for exercise 2
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/operatii-memorie-gdb/ex2/ex2.c
+++ laborator/content/operatii-memorie-gdb/ex2/ex2.c
@@ -2,14 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 
-char* delete_first(char *s, char *pattern);
+char* delete_first(char *s, char *pattern) {
+	char *s1 = malloc((strlen(s) + 1) * sizeof(char));
+	strcpy(s1, s);
+	char *p1 = strtok(s, pattern);
+	char *p2 = strstr(s1, pattern) + strlen(pattern);
+	char *aux = malloc((strlen(p1) + strlen(p2) + 2) * sizeof(char));
+	strcpy(aux, p1);
+	strcat(aux,p2);
+
+	free(s1);
+
+	return aux;
+}
 
 int main(){
-	char *s = "Ana are mere";
+	char s[] = "Ana are mere";
 	char *pattern = "re";
 
-	// Decomentati linia dupa ce ati implementat functia delete_first.
-	// printf("%s\n", delete_first(s, pattern));
+	char *p = delete_first(s, pattern);
+	printf("%s\n", p);
+	free(p);
 
 	return 0;
 }
