bd3659b86c03cb8311837d655d003cde4e5309f1
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-03-24 12:26:47 +0200

    Laborator 3 : exercitiu 6: Add solutions for exercise 6
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/etapele-compilarii/06-obj-link-dev/main.c
+++ laborator/content/etapele-compilarii/06-obj-link-dev/main.c
@@ -1,12 +1,11 @@
+extern qty;
+
 int main(void)
 {
-	/*
-	 * TODO: Make it so you print:
-	 *    price is 21
-	 *    quantity is 42
-	 * without directly calling a printing function from an existing
-	 * library(do NOT use printf, fprintf, fwrite, ...).
-	 */
-
+	int price;
+	price=set_price(21);
+	print_price(price);
+	qty=2*price;
+	print_quantity(&qty);
 	return 0;
 }
