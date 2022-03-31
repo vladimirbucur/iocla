b8eb24f1bef5fd55b3b8fef6eedefcb7167114c5
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-04-01 00:39:07 +0300

    Laborator 4: exercise 3.1: Add solutin for exercise 3.1
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
+++ laborator/content/toolchain-decompilare/3-goto-algs/binary_search.c
@@ -3,12 +3,27 @@
 int main(void)
 {
 	int v[] =  {1, 2, 15, 51, 53, 66, 202, 7000};
-	int dest = v[2]; /* 15 */
+	int dest = 15; /* 15 */
 	int start = 0;
 	int end = sizeof(v) / sizeof(int) - 1;
 
-	/* TODO: Implement binary search */
-	(void) dest;
-	(void) start;
-	(void) end;
+L:
+	if (start > end)
+		goto notfind;
+	int m = start + (end - start) / 2;
+
+	if (v[m] == dest) {
+		printf("Element is present at index %d\n", m);
+		return 0;
+	}
+		
+
+	if (v[m] < dest)
+		start = m + 1;
+	else
+		end = m - 1;
+	goto L;
+
+notfind:
+	printf("Element is not present in array");
 }
