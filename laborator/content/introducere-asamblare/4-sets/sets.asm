decf1f07496b40d1334968cadecfd6d3ea749ab6
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-04-08 01:36:24 +0300

    Laborator 5: exercise 4: Add solutin for exercise 4
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/introducere-asamblare/4-sets/sets.asm
+++ laborator/content/introducere-asamblare/4-sets/sets.asm
@@ -12,22 +12,38 @@ main:
     PRINTF32 `%u\n\x0`, ebx ; afiseaza cea de-a doua multime
 
     ; TODO1: reuniunea a doua multimi
-
+    mov ecx, eax
+    or ecx, ebx
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO2: adaugarea unui element in multime
-
+        ; adaugarea elementelor 3 4
+    or eax, 8
+    or eax, 16
+    PRINTF32 `%u\n\x0`, eax
 
     ; TODO3: intersectia a doua multimi
-
+    mov edx, eax
+    and edx, ebx
+    PRINTF32 `%u\n\x0`, edx
 
     ; TODO4: complementul unei multimi
-
+    mov ecx, eax
+    not ecx
+    PRINTF32 `%u\n\x0`, ecx
 
     ; TODO5: eliminarea unui element
-
+    ; elimin {3}
+    mov edx, 8    ; (2^3)
+    not edx
+    and eax, edx
+    PRINTF32 `%u\n\x0`, eax
 
     ; TODO6: diferenta de multimi EAX-EBX
-
+    mov edx, eax
+    and edx, ebx
+    xor edx, eax
+    PRINTF32 `%u\n\x0`, edx
 
     xor eax, eax
     ret
