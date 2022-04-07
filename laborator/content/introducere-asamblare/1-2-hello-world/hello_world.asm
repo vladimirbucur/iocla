052e38f96ce0966df3a5e3731c867233b17cd8e5
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-04-08 01:26:12 +0300

    Laborator 5: exercises 1-2: Add solutin for exercises 1-2
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
+++ laborator/content/introducere-asamblare/1-2-hello-world/hello_world.asm
@@ -2,22 +2,29 @@
 
 section .data
     myString: db "Hello, World!", 0
+    myString1: db "Goodbye, World!", 0
 
 section .text
     global main
     extern printf
 
 main:
+    mov edx, 0
     mov ecx, 6                      ; N = valoarea registrului ecx
-    mov eax, 1
+    mov eax, 2
     mov ebx, 1
     cmp eax, ebx
-    je print                        ; TODO1: eax > ebx?
+    jg print                        ; TODO1: eax > ebx?
     ret
 
 print:
-    PRINTF32 `%s\n\x0`, myString
-                                    ; TODO2.2: afisati "Hello, World!" de N ori
-                                    ; TODO2.1: afisati "Goodbye, World!"
+    cmp edx, ecx
+    jne loop
+    PRINTF32 `%s\n\x0`, myString1   ; TODO2.2: afisati "Hello, World!" de N ori
+    ret                             ; TODO2.1: afisati "Goodbye, World!"
+    
 
-    ret
+loop:
+    PRINTF32 `%s\n\x0`, myString
+    add edx, 1
+    jmp print
