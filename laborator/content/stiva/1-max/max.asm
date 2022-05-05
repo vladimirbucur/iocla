5fbc36c71059a2f3aa5f40a396e133d72906a255
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-05-06 01:53:33 +0300

    Laborator 8: exercise 1: Add solutin for exercise 1
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/stiva/1-max/max.asm
+++ laborator/content/stiva/1-max/max.asm
@@ -11,6 +11,17 @@ main:
 
     ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
 
+    cmp eax, ebx
+    jl less
+    jmp print
+
+less:
+    push eax
+    push ebx
+    pop eax
+    pop ebx
+
+print:
     PRINTF32 `Max value is: %d\n\x0`, eax ; print maximum value
 
     ret
