316494e2e472fd2babe6686eff2bd03d4c2da32c
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-05-13 03:11:08 +0300

    Laborator 9: exercises 4-5: Add solutin for exercises 4-5
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
+++ laborator/content/apel-functii/4-5-print-string-len/print-string-len.asm
@@ -2,12 +2,36 @@
 
 section .data
     mystring db "This is my string", 0
-
+    form db "String length is %d", 10, 0
+    form2 db "The reversed string is : %s", 10, 0
+    store_string times 64 db 0
+    len dd 0
 section .text
-
 extern puts
 extern printf
 global main
+
+reverse_string:
+    push ebp
+    mov ebp, esp
+    mov eax, dword [ebp+8]
+    mov ecx, dword [ebp+12]
+    add eax, ecx
+    dec eax
+    mov edx, dword [ebp+16]
+
+copy_one_byte:
+    mov bl, byte [eax]
+    mov byte [edx], bl
+    dec eax
+    inc edx
+    loopnz copy_one_byte
+
+    inc edx
+    mov byte [edx], 0
+
+    leave
+    ret
 
 main:
     push ebp
@@ -25,6 +49,20 @@ test_one_byte:
 
 out:
     PRINTF32 `[PRINTF32]: %d\n[printf]: \x0`, ecx
+    mov dword[len], ecx
+    push ecx
+    push form
+    call printf
+    add esp, 8
 
+    push store_string
+    push dword[len]
+    push mystring
+    call reverse_string
+    add esp, 12
+    push store_string
+    push form2
+    call printf
+    add esp, 8
     leave
     ret
