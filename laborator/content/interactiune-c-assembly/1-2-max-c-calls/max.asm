e98daceca1ed98ba0930962ecd7ea43522a5dad1
Vladimir Bucur <vladimirbucur1@gmail.com>
2022-05-20 02:39:12 +0300

    Laborator 10: exercises 1-2: Add solutin for exercises 1-2
    Signed-off-by: Vladimir Bucur <vladimirbucur1@gmail.com>

--- laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
+++ laborator/content/interactiune-c-assembly/1-2-max-c-calls/max.asm
@@ -8,21 +8,30 @@ get_max:
 
 	; save ebx in callee
 	push ebx
+	push edx
 
 	; [ebp+8] is array pointer
 	; [ebp+12] is array length
+	; [ebp + 16] is max position
 
 	mov ebx, [ebp+8]
 	mov ecx, [ebp+12]
+	mov edx, [ebp+16]
 	xor eax, eax
 
 compare:
 	cmp eax, [ebx+ecx*4-4]
 	jge check_end
 	mov eax, [ebx+ecx*4-4]
+	mov [edx], ecx
 check_end:
 	loopnz compare
 
+	mov ecx, [edx]
+	dec ecx
+	mov [edx], ecx
+
+	pop edx
 	pop ebx
 
 	leave
